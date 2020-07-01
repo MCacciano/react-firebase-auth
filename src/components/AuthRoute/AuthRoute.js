6a80ac61148450409e080601cf20de618b7d1324
mcacciano <michael.cacciano@gmail.com>
2020-07-01 09:07:53 -0400

    figuring out the best way to handle the firebase flash

--- src/components/AuthRoute/AuthRoute.js
+++ src/components/AuthRoute/AuthRoute.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 import UserContext from '../../context/userContext';
@@ -6,9 +6,20 @@ import UserContext from '../../context/userContext';
 import './AuthRoute.css';
 
 const AuthRoute = ({ component: Component, ...props }) => {
+  const [gate, setGate] = useState(true);
   const { user } = useContext(UserContext);
 
-  if (!user) {
+  // ! there has to be a better way to handle the flashing of
+  // ! of non auth components while waiting for firebase auth
+  useEffect(() => {
+    setTimeout(() => {
+      if (user === null) {
+        setGate(false);
+      }
+    }, 2000);
+  }, [user]);
+
+  if (gate) {
     return (
       <div className='loading-container'>
         <div className='loading-ripple'>
